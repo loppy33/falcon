a25079e9dd58b3fdb335fc14e97e985799087091
loppy33 <nikita.petrenko1406@gmail.com>
2024-06-10 13:31:25 +0200

    de

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
     return (
         <>
             <div className="container">
-                {account.status !== "connected" ?
+                {account.status === "connected" ?
 
                     <>
                         <header>
