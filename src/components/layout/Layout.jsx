918fced848c3c7859313d6981352e3a4d4de2362
loppy33 <nikita.petrenko1406@gmail.com>
2024-10-08 20:17:31 +0200

    add phone

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -55,15 +55,22 @@ const Layout = () => {
                         </header>
 
                     </> : <>
-                        <nav className="sidebar">
+                        <nav
+                            className="sidebar"
+                            style={
+                                window.innerWidth <= 720 // Проверка на ширину экрана (обычно мобильные экраны)
+                                    ? menu
+                                        ? { transform: 'translateX(0%)' }
+                                        : { transform: 'translateX(-100%)' }
+                                    : {}
+                            }
+                        >
                             <Link className="title" to="/"><img src="/images/icons/logocon.svg" alt="" /></Link>
                             <ul>
                                 <li className={isActive("/") ? "active" : ""}><Link to="/"><img src="/images/icons/home.svg" alt="" /> Home</Link></li>
                                 <li className={isActive("/academy") ? "active" : ""}><Link to="/academy"><img src="/images/icons/academy.svg" alt="" /> Academy</Link></li>
-                                {/* <li className={isActive("/wallets") ? "active" : ""}><Link to="/wallets"><img src="/images/icons/launchpad.svg" alt="" /> Wallets</Link></li> */}
                                 <li className={isActive("/launchpad") ? "active" : ""}><Link to="/launchpad"><img src="/images/icons/launchpad.svg" alt="" /> Launchpad</Link></li>
                                 <li className={isActive("/mylicense") ? "active" : ""}><Link to="/mylicense"><img src="/images/icons/lic.svg" alt="" /> My license</Link></li>
-                                {/* <li className={isActive("/myprojects") ? "active" : ""}><Link to="/myprojects"><FaProjectDiagram />My Projects</Link></li> */}
                                 <li className={isActive("/mystructure") ? "active" : ""}><Link to="/mystructure"><img src="/images/icons/structure.svg" alt="" />My Structure</Link></li>
                                 <li className={isActive("/profile") ? "active" : ""}><Link to="/profile"><img src="/images/icons/profile.svg" alt="" /> Profile</Link></li>
                             </ul>
@@ -72,14 +79,24 @@ const Layout = () => {
                 <main>
                     {account.status === "connected" ?
                         <>
-                            <div className="user">
+                            <div className="user" 
+                            style={
+                                window.innerWidth <= 720 // Проверка на ширину экрана (обычно мобильные экраны)
+                                    ? menu
+                                        ? { backgroundColor: '#101729D1' }
+                                        : { backgroundColor: '#101729' }
+                                    : {}
+                            }>
                                 <div className="pageContainer">
                                     {/* <span>{getPageName(location.pathname)}</span> */}
-                                    <Link className="btn__active" to="/wallets"><img className="icons" src="images/icons/wallet.svg" alt="" />Wallet</Link>
+                                    <Link className="btn__active wallets" to="/wallets"><img className="icons" src="images/icons/wallet.svg" alt="" />Wallet</Link>
 
-                                    <div>
-                                        <img className="icons" src="images/icons/lang.svg" alt="" />
+                                    <div className="lang__and__acc">
+                                        <img className="icons lang" src="images/icons/lang.svg" alt="" />
                                         <w3m-button />
+                                    </div>
+                                    <div className="menu__connected" onClick={() => setMenu(!menu)}>
+                                        <span></span>
                                     </div>
                                 </div>
                             </div>
